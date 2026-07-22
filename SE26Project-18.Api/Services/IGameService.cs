e87260361ba5559b28abba43e66fe1c203634598
Maltuned <93366687+maltuned@users.noreply.github.com>
2026-07-23 06:09:08 +0800

    feat: add game service

---
+++ SE26Project-18.Api/Services/IGameService.cs
@@ -0,0 +1,14 @@
+using SE26Project_18.Api.Models.Requests;
+using SE26Project_18.Api.Models.Responses;
+
+namespace SE26Project_18.Api.Services;
+
+public interface IGameService
+{
+    Task<IReadOnlyCollection<GameResponse>> SearchAsync(
+        SearchGamesRequest request,
+        CancellationToken ct
+    );
+
+    Task<GameResponse?> GetById(long id, CancellationToken ct);
+}
